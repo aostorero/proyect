63ff329bc3a82440910262d1893dff3714fe7c47
aostorero <aostorero99@gmail.com>
2021-02-12 16:55:48 -0300

    small changes to work with frontend

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 //middlewares
 const app = express();
+app.use(cors());
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 app.use('/', routes);
